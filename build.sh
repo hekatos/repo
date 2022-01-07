f08a66725cb2b66d5b8211c8eead598d6dff8b6d
beerpiss <lacvtg.a1.2023@gmail.com>
2022-01-07 20:30:26 +0700

    UconnectBypass

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+script_full_path=$(dirname "$0")
+cd "$script_full_path" || exit 1
+rm Packages Packages.bz2 Packages.xz Packages.zst Release Release.gpg
+
+echo "[Repository] Generating Packages..."
+apt-ftparchive packages ./pool > Packages
+zstd -q -c19 Packages > Packages.zst
+xz -c9 Packages > Packages.xz
+bzip2 -c9 Packages > Packages.bz2
+
+echo "[Repository] Generating Release..."
+apt-ftparchive \
+        -o APT::FTPArchive::Release::Origin="Hekatos" \
+        -o APT::FTPArchive::Release::Label="Hekatos" \
+        -o APT::FTPArchive::Release::Suite="./" \
+        -o APT::FTPArchive::Release::Version="1.0" \
+        -o APT::FTPArchive::Release::Codename="ios" \
+        -o APT::FTPArchive::Release::Architectures="iphoneos-arm" \
+        -o APT::FTPArchive::Release::Description="Combatting jailbreak detection, one tweak at a time" \
+        release . > Release
+
+echo "[Repository] Signing Release using GPG Key..."
+gpg -abs -u DF9111724E146D6F2D08314A95352E56CAF54985 -o Release.gpg Release
+
+echo "[Repository] Finished"
