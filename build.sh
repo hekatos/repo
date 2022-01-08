044694f007bdea0c51c976cd57cdb5fb77a50cbc
beerpiss <lacvtg.a1.2023@gmail.com>
2022-01-09 06:49:48 +0700

    im stupid

--- build.sh
+++ build.sh
@@ -5,15 +5,15 @@ OUTPUT_DIR="publish"
 script_full_path=$(dirname "$0")
 cd "$script_full_path" || exit 1
 rm $OUTPUT_DIR/Packages* $OUTPUT_DIR/*Release*
-mkdir $OUTPUT_DIR/
+mkdir -p $OUTPUT_DIR
 
 echo "[Repository] Generating Packages..."
 apt-ftparchive packages ./pool > $OUTPUT_DIR/Packages
-zstd -q -c19 Packages > $OUTPUT_DIR/Packages.zst
-xz -c9 Packages > $OUTPUT_DIR/Packages.xz
-bzip2 -c9 Packages > $OUTPUT_DIR/Packages.bz2
-gzip -nc9 Packages > $OUTPUT_DIR/Packages.gz
-lzma -c9 Packages > $OUTPUT_DIR/Packages.lzma
+zstd -q -c19 $OUTPUT_DIR/Packages > $OUTPUT_DIR/Packages.zst
+xz -c9 $OUTPUT_DIR/Packages > $OUTPUT_DIR/Packages.xz
+bzip2 -c9 $OUTPUT_DIR/Packages > $OUTPUT_DIR/Packages.bz2
+gzip -nc9 $OUTPUT_DIR/Packages > $OUTPUT_DIR/Packages.gz
+lzma -c9 $OUTPUT_DIR/Packages > $OUTPUT_DIR/Packages.lzma
 
 echo "[Repository] Generating Release..."
 apt-ftparchive \
@@ -25,7 +25,7 @@ apt-ftparchive \
         -o APT::FTPArchive::Release::Architectures="iphoneos-arm" \
         -o APT::FTPArchive::Release::Components="main" \
         -o APT::FTPArchive::Release::Description="Combatting jailbreak detection, one tweak at a time" \
-        release . > $OUTPUT_DIR/Release
+        release $OUTPUT_DIR > $OUTPUT_DIR/Release
 
 echo "[Repository] Signing Release using GPG Key..."
 if ! gpg -vabs -u $GPG_KEY -o $OUTPUT_DIR/Release.gpg $OUTPUT_DIR/Release; then
